1974dea6592c3fbe13e6600632152e0102fffa9e
uo276026 <uo276026@uniovi.es>
2026-08-24 14:26:44 +0200

    logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     redirect_back fallback_location: root_path
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   private
 
   def set_locale

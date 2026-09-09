7e3e10baa363e737e9e5f7e979382e751f90dd96
uo276026 <uo276026@uniovi.es>
2026-09-09 14:49:05 +0200

    small fix

--- app/jobs/chat_entry_broadcast_job.rb
+++ app/jobs/chat_entry_broadcast_job.rb
@@ -1,7 +1,8 @@
 class ChatEntryBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(chat_entry, sender_id)
+  def perform(chat_entry_id, sender_id)
+    chat_entry = ChatEntry.find(chat_entry_id)
     chat = chat_entry.chat
     chat.users.where.not(id: sender_id).find_each do |user|
       BroadcastHelper.add_message_to_chat(chat_entry, user)
